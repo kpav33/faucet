432873576dd22f2fb34b10d0ade1f645ce0b84f8
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-04-21 12:56:38 +0200

    Access smart contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,8 +2,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables
-    uint public funds = 1000; // positive values only
-    int public counter = -10;
-    uint public test = 4294967295;
+
+  // this is a special function
+  // it's called when you make a transaction that doesn't specify
+  // function name to call
+
+  // External functions are part of the contract interface
+  // which means they can be called via contracts and other transactions
+
+    receive() external payable {}
 }
+
+// Block info
+// Nonce - a hash that when combined with the minHash proofs that
+// the block has gone through proof of work(POW)
+// 8 bytes => 64 bits
