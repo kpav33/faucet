6928df5483b9f58abe735bd0dd81980cdf9f6134
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-04-25 12:03:14 +0200

    Add mappings to contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -7,7 +7,12 @@ contract Faucet {
   // private -> can be accesible only within the smart contract
   // internal -> can be accesible within smart contract and also derived smart contract
   // address[] public funders;
-  address[] private funders;
+  // address[] private funders;
+
+    uint public numOfFunders;
+    // mapping(uint => address) private funders;
+    mapping(address => bool) private funders;
+    mapping(uint => address) private lutFunders;
 
   // this is a special function
   // it's called when you make a transaction that doesn't specify
@@ -21,8 +26,43 @@ contract Faucet {
     // Payable functions can process transactions
     function addFunds() external payable {
       // Push the senders address into the funders array
-      funders.push(msg.sender);
+      // funders.push(msg.sender);
+      // uint index = numOfFunders++;
+      // funders[index] = msg.sender;
+      address funder = msg.sender;
+
+      if (!funders[funder]) {
+        numOfFunders++;
+        uint index = numOfFunders++;
+        funders[funder] = true;
+        lutFunders[index] = funder;
+      }
     }
+
+    function getAllFunders() external view returns (address[] memory) {
+      address[] memory _funders = new address[](numOfFunders);
+
+      for (uint i = 0; i < numOfFunders; i++) {
+        _funders[i] = lutFunders[i];
+      }
+
+      return _funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns(address) {
+      return lutFunders[index];
+    }
+
+    
+    // function getAllFunders() external view returns (address[] memory) {
+    //   address[] memory _funders = new address[](numOfFunders);
+
+    //   for (uint i = 0; i < numOfFunders; i++) {
+    //     _funders[i] = funders[i];
+    //   }
+
+    //   return _funders;
+    // }
 
     // External function that views value in contract storage and returns it
     // You can't call functions marked as external from inside of the smart contract
@@ -33,15 +73,22 @@ contract Faucet {
     // Functions marked as public cant be called by other functions inside of the smart contract
     // Use external for functions that you expect to only call from outside of the smart contract
     // Use public for functions that you expect to call from inside and outside of the smart contract
-    function getAllFunders() public view returns (address[] memory) {
-      return funders;
-    }
+    // function getAllFunders() public view returns (address[] memory) {
+    //   return funders;
+    // }
 
-    function getFunderAtIndex(uint8 index) external view returns (address) {
-      address[] memory _funders = getAllFunders();
-      return _funders[index];
-    }
+    // function getFunderAtIndex(uint8 index) external view returns (address) {
+    //   // address[] memory _funders = getAllFunders();
+    //   // return _funders[index];
+    //   return funders[index];
+    // }
 }
+
+// const instance = await Faucet.deployed();
+// instance.addFunds({from: accounts[0], value: "200000000"})
+// instance.addFunds({from: accounts[1], value: "200000000"})
+// instance.getFunderAtIndex(0)
+// instance.getAllFunders()
 
  // const instance = await Faucet.deployed()
  // instance.addFunds({ from: accounts[0], value: "2" })
